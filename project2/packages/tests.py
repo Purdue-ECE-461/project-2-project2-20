a01de2cd8abf30f93a5897df4a3b4257d8995395
Vikram Srivastava <vikramsrivastava1729@gmail.com>
2021-12-08 21:55:30 -0500

    added more tests

--- project2/packages/tests.py
+++ project2/packages/tests.py
@@ -12,8 +12,8 @@ class PackageTestCase(TestCase):
         u.is_superuser = True
         u.is_staff = True
         u.save()
-        Package.objects.create(name='lodash', version='4.0.0', url='https://github.com/lodash/lodash', owner=u)
-        Package.objects.create(name='jQuery', version='3.6.0', url='https://github.com/jquery/jquery', owner=u)
+        Package.objects.create(name='lodash', version='4.0.0', url='https://github.com/lodash/lodash')
+        Package.objects.create(name='jQuery', version='3.6.0', url='https://github.com/jquery/jquery')
 
     def test_num_packages(self):
         packages = list(Package.objects.all())
@@ -23,6 +23,33 @@ class PackageTestCase(TestCase):
         lodash_package = Package.objects.get(id=1)
         self.assertEqual(lodash_package.name, 'lodash')
 
-    def checkRating(self):
-        pass
+class RESTTestCase(APITestCase):
+    def checkReset(self):
+        response = self.client.delete('/api/reset/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def checkCreate(self):
+        data = {'name': 'sample', 'version': '1.0.0', 'url': 'https://github.com/sample/sample/'}
+        self.client.post('/api/package/', data)
+        all_packages_data = self.client.get('/api/packages/')
+        self.assertEqual(len(all_packages_data), 1)
+
+    def checkUpdate(self):
+        data = {'name': 'sample', 'version': '1.0.0', 'url': 'https://github.com/sample/sample/'}
+        self.client.post('/api/package/', data)
+        new_data = {'name': 'sample', 'version': '2.0.0', 'url': 'https://github.com/sample/sample/'}
+        response = self.client.put('/api/package/', new_data)
+        self.assertEqual(response.status_code, HTTP_200_OK)
+
+    def checkDelete(self):
+        data = {'name': 'sample', 'version': '1.0.0', 'url': 'https://github.com/sample/sample/'}
+        self.client.post('/api/package/', data)
+        response = self.client.delete('/api/package')
+        self.assertEqual(response.status_code, HTTP_200_OK)
+
+    def checkRate(self):
+        data = {'name': 'sample', 'version': '1.0.0', 'url': 'https://github.com/sample/sample/'}
+        self.client.post('/api/package/', data)
+        response = self.client.get('/api/package/1/rate')
+        self.assertEqual(response.status_code, HTTP_200_OK)
 
