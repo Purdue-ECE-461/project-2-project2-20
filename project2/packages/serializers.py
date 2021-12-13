8ca5faa221f3a9693856ffaf52345f968c31890d
Vikram Srivastava <vikramsrivastava1729@gmail.com>
2021-12-12 21:29:31 -0500

    add user signup back

--- project2/packages/serializers.py
+++ project2/packages/serializers.py
@@ -12,6 +12,15 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id', 'username', 'password', 'is_staff']
 
+    def create(self, validated_data):
+        # method borrowed from https://stackoverflow.com/questions/27586095/why-isnt-my-django-user-models-password-hashed
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
+
 class RatingSerializer(serializers.Serializer):
     BusFactor = serializers.DecimalField(max_digits=3, decimal_places=1)
     Correctness = serializers.DecimalField(max_digits=3, decimal_places=1)
